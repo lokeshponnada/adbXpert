f624aa3b14c0cfc901b2bdd3162a6ebf38c55733
GitHub Action <action@github.com>
2023-09-17 02:05:46 +0000

    Template cleanup

--- src/main/kotlin/com/github/lokeshponnada/adbxpert/services/MyProjectService.kt
+++ src/main/kotlin/com/github/lokeshponnada/adbxpert/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.lokeshponnada.adbxpert.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.lokeshponnada.adbxpert.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
